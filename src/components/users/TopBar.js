2064ef427a3b8c91a7c6a947168484d693c79af0
Pradeep <pradeep.raj922@gmail.com>
2023-07-07 21:53:36 +0530

    Login

--- src/components/users/TopBar.js
+++ src/components/users/TopBar.js
@@ -3,12 +3,16 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import {useNavigate} from 'react-router-dom'
+import { LinkContainer } from 'react-router-bootstrap';
+
 function TopBar(props) {
   let navigate = useNavigate()
   return <>
   <Navbar bg="primary" variant="dark">
     <Container>
-      <Navbar.Brand href="javascript(void)">FoodApp</Navbar.Brand>
+    <LinkContainer to='javascript(void)'>
+      <Navbar.Brand>FoodApp</Navbar.Brand>
+      </LinkContainer>
       <Nav className="me-auto">
         <Nav.Link onClick={()=>navigate('/user-menu')}>Home</Nav.Link>
         <Nav.Link onClick={()=>navigate('/user-cart')}>Cart {props.value.cart.length}</Nav.Link>

c4918e687379e3e093633d63c539f9d20b85582f
Pradeep <pradeep.raj922@gmail.com>
2023-07-09 12:39:30 +0530

    Logout

--- src/components/admin/AdminNav.js
+++ src/components/admin/AdminNav.js
@@ -2,17 +2,22 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import {useNavigate} from 'react-router-dom'
+import {useNavigate} from 'react-router-dom';
+import { LinkContainer } from 'react-router-bootstrap';
+
 
 function AdminNav() {
   let navigate = useNavigate()
   return <>
       <Navbar bg="primary" variant="dark">
         <Container>
-          <Navbar.Brand href="javascript(void)">FoodApp</Navbar.Brand>
+        <LinkContainer to='/login'>
+      <Navbar.Brand>FoodApp</Navbar.Brand>
+      </LinkContainer>
           <Nav className="me-auto">
             <Nav.Link onClick={()=>navigate('/dashboard')}>Dashboard</Nav.Link>
             <Nav.Link onClick={()=>navigate('/food-management')}>Food Management</Nav.Link>
+
           </Nav>
         </Container>
       </Navbar>
